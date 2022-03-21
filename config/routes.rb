91f50e8b11e8e5b13a954a24a6daeb3423e2bde2
yakushimizu <ryusou0523@gmail.com>
2022-03-21 13:47:02 +0000

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index, :show, :edit, :create, :destroy] do
+  get 'relationships/followings'
+  get 'relationships/followers'
+  resources :items, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :item_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   patch 'items/:id' => 'items#update', as: 'update_item'
   delete 'items/:id' => 'items#destroy', as: 'destroy_item'
   devise_for :users
